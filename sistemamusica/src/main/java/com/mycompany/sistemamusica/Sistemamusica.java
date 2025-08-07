e05eddaa65325cb45b4b8b92a12c65cb6224f86b
AlexandreTorrecilhas <carlos.torrecilhas@outlook.com>
2025-08-06 21:29:39 -0300

    Strting the project

---
+++ sistemamusica/src/main/java/com/mycompany/sistemamusica/Sistemamusica.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemamusica;
+
+/**
+ *
+ * @author carlo
+ */
+public class Sistemamusica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
